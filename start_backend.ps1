777bf4c8dccd51a2c467e55f15d683c8980da18f
Kishore0752 <kishoreregana702@gmail.com>
2026-02-28 14:11:50 +0530

    addedreadme file

--- start_backend.ps1
+++
@@ -1,5 +0,0 @@
-# PowerShell script to start the backend server
-Set-Location -Path "C:\Users\MR\OneDrive\Desktop\Learning\backend"
-Write-Host "Changed to: $(Get-Location)"
-Write-Host "Starting server..."
-& node server.js
